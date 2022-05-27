ef211772f37f4fc6ad22187f357ecf08d7aace98
Zera-tool <array_array@hotmail.com>
2022-05-27 15:05:57 +0200

    .

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,12 +1,11 @@
-import xleft from './images/x-men.jpg';
-import xright from './images/x-men3.jpg';
+import xleft from "./images/x-men.jpg";
 
 const Header = () => {
-    return (
-        <header>
-            <img src={xleft} alt={"xmen"} /><img className='floor-pic' src={xright} alt={"xmen"} />
-        </header>
-    )
-}
+  return (
+    <header>
+      <img src={xleft} alt={"xmen"} />
+    </header>
+  );
+};
 
-export default Header
+export default Header;
